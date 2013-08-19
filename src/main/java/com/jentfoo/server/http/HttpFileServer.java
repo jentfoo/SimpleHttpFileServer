d45588e1aebae7be4965547e0a6dead104ac1fd6
Mike Jensen <mike.jensen@readytalk.com>
2013-08-19 13:29:18 -0600

    More implementation for starting and handling requests

--- src/main/java/com/jentfoo/server/http/HttpFileServer.java
+++ src/main/java/com/jentfoo/server/http/HttpFileServer.java
@@ -1,35 +1,50 @@
 package com.jentfoo.server.http;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class HttpServer {
+public class HttpFileServer {
   private static final int MAX_CONCURRENT_REQUESTS = 100000;
   
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    if (args.length < 2) {
+      System.err.println("Expected at least two arguments: <root file path> <port> [bind host]");
+      System.exit(1);
+    }
+    File fileRoot = new File(args[0]);
+    int bindPort = Integer.parseInt(args[1]);
     
+    HttpFileServer server;
+    if (args.length > 2) {
+      server = new HttpFileServer(bindPort, args[2], fileRoot);
+    } else {
+      server = new HttpFileServer(bindPort, fileRoot);
+    }
+    
+    server.startServer();
   }
   
   private final InetSocketAddress bindAddress;
   private final RequestHandler requestHandler;
   
-  public HttpServer(int port) {
-    this(new InetSocketAddress(port));
+  public HttpFileServer(int port, File fileRoot) {
+    this(new InetSocketAddress(port), fileRoot);
   }
   
-  public HttpServer(int port, String bindHost) {
-    this(new InetSocketAddress(bindHost, port));
+  public HttpFileServer(int port, String bindHost, File fileRoot) {
+    this(new InetSocketAddress(bindHost, port), fileRoot);
   }
   
-  public HttpServer(InetSocketAddress bindAddress) {
+  public HttpFileServer(InetSocketAddress bindAddress, File fileRoot) {
     if (bindAddress == null) {
       throw new IllegalArgumentException("Must provide InetSocketAddress to bind on");
     }
     
     this.bindAddress = bindAddress;
-    this.requestHandler = new RequestHandler(MAX_CONCURRENT_REQUESTS);
+    this.requestHandler = new RequestHandler(MAX_CONCURRENT_REQUESTS, fileRoot);
   }
   
   public void startServer() throws IOException {
--- src/main/java/com/jentfoo/server/http/RequestHandler.java
+++ src/main/java/com/jentfoo/server/http/RequestHandler.java
@@ -1,5 +1,9 @@
 package com.jentfoo.server.http;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 import org.threadly.concurrent.PriorityScheduledExecutor;
@@ -7,29 +11,73 @@ import org.threadly.concurrent.PriorityScheduledExecutor;
 public class RequestHandler {
   private static final int THREAD_KEEP_ALIVE_TIME_IN_MILLIS = 1000 * 15;
   
+  private final File fileRoot;
   private final PriorityScheduledExecutor scheduler;
   
-  protected RequestHandler(int maxConcurrentRequests) {
+  protected RequestHandler(int maxConcurrentRequests, File fileRoot) {
+    if (maxConcurrentRequests < 1) {
+      throw new IllegalArgumentException("Must allow at least one concurrent request");
+    } else if (fileRoot == null) {
+      throw new IllegalArgumentException("Must provide file root to serve files from");
+    } else if (! fileRoot.exists()) {
+      throw new IllegalArgumentException("File root does not exist: " + fileRoot.getAbsolutePath());
+    } else if (! fileRoot.isDirectory()) {
+      throw new IllegalArgumentException("File root must be a directory: " + fileRoot.getAbsolutePath());
+    }
+    
+    this.fileRoot = fileRoot;
+    
     int corePoolSize = Math.min(maxConcurrentRequests, Runtime.getRuntime().availableProcessors() * 2);
     scheduler = new PriorityScheduledExecutor(corePoolSize, maxConcurrentRequests, 
                                               THREAD_KEEP_ALIVE_TIME_IN_MILLIS);
   }
 
-  public void handleRequest(Socket requestSocket) {
+  public void handleRequest(Socket requestSocket) throws IOException {
     scheduler.execute(new RequestWorker(requestSocket));
   }
   
   private static class RequestWorker implements Runnable {
     private final Socket requestSocket;
+    private final InputStream requestIn;
+    private final OutputStream requestOut;
 
-    public RequestWorker(Socket requestSocket) {
+    public RequestWorker(Socket requestSocket) throws IOException {
       this.requestSocket = requestSocket;
+      requestIn = requestSocket.getInputStream();
+      requestOut = requestSocket.getOutputStream();
     }
 
     @Override
     public void run() {
-      // TODO Auto-generated method stub
-      
+      try {
+        System.out.println("Got request from: " + requestSocket.getRemoteSocketAddress());
+        int val;
+        while ((val = requestIn.read()) != -1) {
+          System.out.print((char)val);
+        }
+      } catch (IOException e) {
+          e.printStackTrace(System.err);
+      } finally {
+        close();
+      }
+    }
+    
+    private void close() {
+      try {
+        requestIn.close();
+      } catch (IOException e) {
+        e.printStackTrace(System.err);
+      }
+      try {
+        requestOut.close();
+      } catch (IOException e) {
+        e.printStackTrace(System.err);
+      }
+      try {
+        requestSocket.close();
+      } catch (IOException e) {
+        e.printStackTrace(System.err);
+      }
     }
   }
 }
